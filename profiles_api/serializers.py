2202ec40b368ccdc1a2ba28fb64035b4333e4ab6
Anmol Goyal <magicalanmol100@gmail.com>
2020-09-26 12:32:45 +0530

    Added viewset to PROFILES api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,3 +3,5 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView and can also handle validations"""
     name=serializers.CharField(max_length=10)
+
+    """It tells us which fields are we expecting in the request body"""
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,12 @@
 from . import views
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+"""Registering a specific viewset with our router"""
+router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
 
 urlpatterns=[
-    path('hello-view/',views.HelloApiView.as_view())
+    path('hello-view/',views.HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
